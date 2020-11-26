a7cb155aa713ea27b4845cdb8b9a2f995aab617a
Luciano Silva <luciano2023silva@gmail.com>
2020-11-25 21:56:32 -0300

    alterando profile e apiClient

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { Console } from 'console';
-console.log(process.env.REACT_APP_API_URL)
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
